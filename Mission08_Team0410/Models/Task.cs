fdf00a53079e0d80f94e3e6a6efa104b742cb69d
zachedge <103464298+zachedge@users.noreply.github.com>
2024-02-27 16:52:13 -0700

    zwe push 1

---
+++ Mission08_Team0410/Models/Task.cs
@@ -0,0 +1,12 @@
+﻿namespace Mission08_Team0410.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Quadrant { get; set; }
+        public int CategoryId { get; set; }
+        public bool Completed { get; set; }
+    }
+}
