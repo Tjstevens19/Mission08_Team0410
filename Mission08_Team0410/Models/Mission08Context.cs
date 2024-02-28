b496ff66fce7f20f47587cc12b60dd922dc51702
zachedge <103464298+zachedge@users.noreply.github.com>
2024-02-27 17:28:00 -0700

    everything pre migrations
    program.cs, changed context name, appsettings.json

---
+++ Mission08_Team0410/Models/Mission08Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission08_Team0410.Models
+{
+    public class Mission08Context : DbContext
+    {
+        public Mission08Context(DbContextOptions<Mission08Context> options) : base (options)
+        {
+
+        }
+
+        public DbSet<TaskClass> Schedule {  get; set; }
+    }
+}
