ab5a5e9a1fae5b1dd5f4c58dd543cf06c41a2229
zachedge <103464298+zachedge@users.noreply.github.com>
2024-02-28 17:54:05 -0700

    new changes

--- Mission08_Team0410/Models/iTaskRepository.cs
+++ Mission08_Team0410/Models/iTaskRepository.cs
@@ -3,6 +3,7 @@
     public interface iTaskRepository
     {
         List<Task> Tasks { get; }
+        List<Category> Categories { get; }
         public void AddTask(Task task);
         public void UpdateTask(Task task);
         public void DeleteTask(Task task);
